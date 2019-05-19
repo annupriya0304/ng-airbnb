48e30e83c6540b2d5ecbe6258dd144e779428aa4
Annu Priya <annupriya0304@gmail.com>
2019-05-19 14:18:35 +0530

    Updated header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RentalComponent } from './rental/rental.component';
+import { HeaderComponent } from './Common/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RentalComponent
+    RentalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
